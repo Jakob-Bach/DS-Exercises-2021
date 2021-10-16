ab0fedba6f6eed78a688d513c9bafa1847d5c1a9
Jakob Bach <jakob.bach@kit.edu>
2021-10-16 10:01:55 +0200

    Create preliminary version of 2021's E3 solution
    - extend environment (for dataset loading and association rules)
    - cut stuff not related to current exercise sheet (because kicked out)
    - add more explanation

--- E3/prepare_groceries_dataset.py
+++ E3/prepare_groceries_dataset.py
@@ -2,7 +2,7 @@
 
 Run this script to download, preprocess, and save the Groceries dataset used for exercise sheet 3.
 
-Usage: python procure_groceries_dataset.py
+Usage: python prepare_groceries_dataset.py
 """
 
 import csv
@@ -12,25 +12,28 @@ import warnings
 import pyreadr
 import rdata
 
+
+GROCERIES_URL = 'https://github.com/mhahsler/arules/raw/master/data/Groceries.rda'
+GROCERIES_DIR = './'
+GROCERIES_FILENAME = 'groceries.csv'
+GROCERIES_STRUCTURE_FILENAME = 'groceries_structure.csv'
+
+
 if __name__ == '__main__':
-    GROCERIES_URL = 'https://github.com/mhahsler/arules/raw/master/data/Groceries.rda'
-    TMP_RDA_DIR = './'
-    TMP_RDA_FILENAME = 'groceries.rda'
-    GROCERIES_DIR = 'solution/'
-    GROCERIES_FILENAME = 'groceries.csv'
-    GROCERIES_STRUCTURE_FILENAME = 'groceries_structure.csv'
+    print('Started preparing data.')
 
     # Download and parse:
-    tmp_file_path = os.path.join(TMP_RDA_DIR, TMP_RDA_FILENAME)
+    tmp_file_path = os.path.join(GROCERIES_DIR, 'groceries.rda')
     pyreadr.download_file(GROCERIES_URL, tmp_file_path)
-    parsed = rdata.parser.parse_file(tmp_file_path)
+    parsed_data = rdata.parser.parse_file(tmp_file_path)
     with warnings.catch_warnings():
         warnings.filterwarnings(action='ignore', message='Missing constructor for R class')
-        converted = rdata.conversion.convert(parsed)
+        groceries_data = rdata.conversion.convert(parsed_data)['Groceries']
     os.remove(tmp_file_path)
 
     # Prepare hierarchy of item categories (for multi-level mining):
-    structure_df = converted['Groceries'].itemInfo[['labels', 'level1', 'level2']].rename(columns={'labels': 'label'})
+    structure_df = groceries_data.itemInfo[['labels', 'level1', 'level2']]
+    structure_df = structure_df.rename(columns={'labels': 'label'})
     structure_df.to_csv(os.path.join(GROCERIES_DIR, GROCERIES_STRUCTURE_FILENAME), index=False)
 
     # Prepare transaction data - is a sparse matrix in R (ngCMatrix), thus code slightly awkward:
@@ -40,12 +43,14 @@ if __name__ == '__main__':
     # 2nd transaction etc.), but the boundaries between transactions are not clear from "i" alone
     # - "p" tells at which indices "i" should be split to create the transactions; it is a 1D array
     # whose length corresponds to the number of transactions + 1
-    item_labels = [structure_df.iloc[idx]['label'] for idx in converted['Groceries'].data.i]
+    item_labels = [structure_df.iloc[idx]['label'] for idx in groceries_data.data.i]
     transactions = [item_labels[transaction_start_idx:transaction_end_idx]
                     for transaction_start_idx, transaction_end_idx
-                    in zip(converted['Groceries'].data.p[:-1], converted['Groceries'].data.p[1:])]
+                    in zip(groceries_data.data.p[:-1], groceries_data.data.p[1:])]
 
     with open(os.path.join(GROCERIES_DIR, GROCERIES_FILENAME), mode='w', newline='') as outfile:
         writer = csv.writer(outfile)
         for row in transactions:
             writer.writerow(row)
+
+    print('Finished preparing data.')
